213acbe5d8712653889d867797ee96fc86c506bd
EC2 Default User <ec2-user@ip-172-31-4-222.ap-northeast-1.compute.internal>
2023-04-18 04:37:06 +0000

    [Finish] book/editページ

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only: [:index, :show]
+  resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:index, :show, :edit, :update]
 end
